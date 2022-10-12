6f4246bc46a93ca69adbba49b99fdf5dfb8fce73
9ziggy9 <davidarogers@protonmail.com>
2022-10-12 12:37:34 -0700

    signup modifications

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -2,6 +2,8 @@ from flask import (Blueprint,
                    render_template,
                    redirect,
                    url_for)
+from ..forms import Signup
+from ..models import User
 
 auth = Blueprint("auth", __name__)
 
@@ -11,12 +13,18 @@ def login():
 
 @auth.route("/signup")
 def signup():
-    return render_template("signup.html")
+    form = Signup()
+    return render_template("signup.html", form=form)
 
 @auth.route("/signup", methods=["POST"])
 def signup_post():
-    # TODO: code to validate and add users
-    return redirect(url_for("auth.login"))
+    form = Signup()
+    if form.validate_on_submit():
+        user = User()
+        form.populate_obj(user)
+        print(user)
+        return redirect(url_for("auth.login"))
+    return "Bad Data"
 
 @auth.route("/logout")
 def logout():
