f9b897b04fdab766695ac0bd66c962072fcc7d39
9ziggy9 <davidarogers@protonmail.com>
2022-10-12 11:54:54 -0700

    brought in wtforms

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,15 +1,23 @@
-from flask import Blueprint, render_template
+from flask import (Blueprint,
+                   render_template,
+                   redirect,
+                   url_for)
 
-auth = Blueprint('auth', __name__)
+auth = Blueprint("auth", __name__)
 
-@auth.route('/login')
+@auth.route("/login")
 def login():
     return render_template("login.html")
 
-@auth.route('/signup')
+@auth.route("/signup")
 def signup():
     return render_template("signup.html")
 
-@auth.route('/logout')
+@auth.route("/signup", methods=["POST"])
+def signup_post():
+    # TODO: code to validate and add users
+    return redirect(url_for("auth.login"))
+
+@auth.route("/logout")
 def logout():
-    return 'Logout'
+    return "Logout"
