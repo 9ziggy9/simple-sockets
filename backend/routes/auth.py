041ca5ef87866a84de8e52a647701341766e0ac2
9ziggy9 <davidarogers@protonmail.com>
2022-10-12 14:44:34 -0700

    templating and form corrections

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -2,14 +2,28 @@ from flask import (Blueprint, render_template,
                    redirect, url_for, flash)
 from werkzeug.security import (generate_password_hash,
                                check_password_hash)
-from ..forms import Signup
+from ..forms import Signup, Login
 from ..models import db, User
 
 auth = Blueprint("auth", __name__)
 
 @auth.route("/login")
 def login():
-    return render_template("login.html")
+    form = Login()
+    return render_template("login.html", form=form)
+
+@auth.route("/login", methods=["POST"])
+def login_post():
+    form = Login()
+    if form.validate_on_submit():
+        user = User()
+        form.populate_obj(user)
+        existing_user = User.query.filter_by(name=user.name).first()
+        if not existing_user or not check_password_hash(existing_user.password,
+                                                        user.password):
+            return "LOGIN NO BUENO MY DUDES"
+        return redirect(url_for("main.profile"))
+
 
 @auth.route("/signup")
 def signup():
